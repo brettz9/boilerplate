6eadae5b15027e531b26b96c48629adc87920215
Brett Zamir <brettz9@yahoo.com>
2019-12-19 20:50:36 +0800

    - 2 sps.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true
-    },
-    settings: {
-        polyfills: [
-        ]
-    },
-    extends: ['ash-nazg/sauron', 'plugin:node/recommended-script'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
-    overrides: [
-      {
-        files: '*.md',
-        rules: {
+  env: {
+    browser: true,
+    es6: true
+  },
+  settings: {
+    polyfills: [
+    ]
+  },
+  extends: ['ash-nazg/sauron', 'plugin:node/recommended-script'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  overrides: [
+    {
+      files: '*.md',
+      rules: {
 
-        }
-      },
-      {
-        files: '*.html',
-        rules: {
-          'import/unambiguous': 0
-        }
       }
-    ],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module'
     },
-    rules: {
+    {
+      files: '*.html',
+      rules: {
+        'import/unambiguous': 0
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+  }
 };
